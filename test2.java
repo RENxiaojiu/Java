8410bd9c1f4c8ae457ed02fe0af5853c88224c95
RENxiaojiu <934131814@qq.com>
2021-01-23 21:55:02 +0800

    1到 100 的所有整数中出现多少个数字9

---
+++ test2.java
@@ -0,0 +1,27 @@
+public class Test {
+
+    public static void main(String[] args) {
+
+        int a = 0;
+
+        for(int i= 1;i = 100;i++) {
+
+            if (i % 10 == 9) {
+
+                a++;
+
+            }
+
+            if (i  10 == 9) {
+
+                a++;
+
+            }
+
+        }
+
+            System.out.println(a);
+
+    }
+
+}
