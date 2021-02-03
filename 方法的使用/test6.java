e40b1621e17f836f83800bd4e57f9444083de7d5
RENxiaojiu <934131814@qq.com>
2021-02-03 14:53:21 +0800

    创建方法求两个数的最大值max2，随后再写一个求3个数的最大值的函数max3。 ​ 要求：在max3这个函数中，调用max2函数，来实现3个数的最大值计算

---
+++ 方法的使用/test6.java
@@ -0,0 +1,31 @@
+ public static void main(String[] args) {
+
+    max2();
+
+  }
+
+
+
+  public static void max2() {
+
+    Scanner scanner = new Scanner(System.in);
+
+    int a = scanner.nextInt();
+
+    int b = scanner.nextInt();
+
+    if(a > b){
+
+      System.out.println(a);
+
+    }else if{
+
+      System.out.println(b);
+
+    }else{
+
+      System.out.println("相等");
+
+    }
+
+  }
