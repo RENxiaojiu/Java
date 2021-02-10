96771192b03bd54194f22e2f276418d85ea83a2c
RENxiaojiu <934131814@qq.com>
2021-02-11 01:21:27 +0800

    实现方法 indexOf, 能够找出字符串子串存在的位置

---
+++ String 类/test3.java
@@ -0,0 +1,20 @@
+
+  public static void main(String[] args) {
+
+    indexOf();
+
+  }
+
+
+
+  public static void indexOf() {
+
+    String a = "hello world";
+
+    String b = "world";
+
+    int result = a.indexOf(b);
+
+    System.out.println(result);
+
+  }
