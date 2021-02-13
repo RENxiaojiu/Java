feef2eedae85aadac0bfbfc8d934738df5bb92db
RENxiaojiu <934131814@qq.com>
2021-02-13 23:21:49 +0800

    实现方法 contains, 能够判定字符串中是否包含子串

---
+++ String 类/test4.java
@@ -0,0 +1,17 @@
+  public static void main(String[] args) {
+
+    contains();
+
+  }
+
+
+
+  public static void contains() {
+
+    String a = "hello world";
+
+    String b = "world";
+
+    System.out.println(a.contains(b));
+
+  }
