bc85a17247ab32e80f99916c68739f24e0227ac3
RENxiaojiu <934131814@qq.com>
2021-02-13 23:23:30 +0800

    实现方法 compareTo, 能够实现按照字典序比较字符串大小

---
+++ String 类/test5.java
@@ -0,0 +1,17 @@
+  public static void main(String[] args) {
+
+    compareTo();
+
+  }
+
+
+
+  public static void compareTo() {
+
+    String a = "hello";
+
+    String b = "Hello";
+
+    System.out.println(a.compareTo(b));
+
+  }
