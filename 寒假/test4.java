11d17eaee3aaa8230f8f521c04aaa363e44573f4
RENxiaojiu <934131814@qq.com>
2021-02-25 21:41:09 +0800

    给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。你可以假设数组中无重复元素。

---
+++ 寒假/test4.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (target == nums[i] || target < nums[i]) {
+                return i;
+            } else if (target > nums[nums.length - 1]) {
+                return nums.length;
+            } 
+        }
+        return 0;
+    }
+}
